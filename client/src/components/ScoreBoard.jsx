0aa7d8d42f82c1474ee2d5f5c45ee7fbf6d18c0b
Anastasia <anarenee9696@gmail.com>
2022-01-22 15:11:13 -0600

    updated scoreboard layout

--- client/src/components/ScoreBoard.jsx
+++ client/src/components/ScoreBoard.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const ScoreBoard = () => {
   return (
-    <div>
-      Score Board here!
+    <div id='score-board'>
+      <div className='high-sc'>
+        <h4>HIGH SCORE</h4>
+        get user high-score from db here
+      </div>
+      <div className='game-sc'>
+        <h4>CURRENT SCORE</h4>
+        accumulate score as lines are cleared
+      </div>
+      <button>LEADERBOARD</button>
     </div>
   );
 };
