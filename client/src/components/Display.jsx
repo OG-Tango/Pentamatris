564b281b73c7d323fa9646cf505aa477387f886f
Riley Katz <riley.davis.katz@gmail.com>
2022-01-27 13:45:11 -0600

    merge conflicts resolved

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { StyledDisplay } from "./styles/StyledDisplay";
 
+//The display is a flexible box that can be adjusted for each desired
+//bit of information displayed
+
 const Display = ({ gameOver, text }) => (
   <StyledDisplay gameOver={gameOver}>{text}</StyledDisplay>
 )
